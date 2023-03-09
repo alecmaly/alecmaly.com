eda0659e09355f362d151e76eb0de407981ee35d
Alec Maly <alecjmaly@gmail.com>
2023-03-09 11:08:21 -0500

    hacky way to get PID and kill ssh-agent to prevent memory leak from cronjob

--- rebuild_blog_and_search_index.sh
+++ rebuild_blog_and_search_index.sh
@@ -6,32 +6,31 @@
 
 # these may be needed for github auth
 # - if broken, regenerate key `ssh-keygen -t rsa -b 4096 ` + upload public key to GitHub
-eval "$(ssh-agent -s)"
+eval "$(ssh-agent -s)" > /tmp/ssh_agent_output
+SSH_AGENT_PID=`cat /tmp/ssh_agent_output | cut -d' ' -f3`
+rm /tmp/ssh_agent_output
 ssh-add ~/.ssh/id_rsa
-
-# Capture PID of ssh-agent process
-SSH_AGENT_PID=$!
 
 
 pushd /opt/blog
 git fetch
-neec_to_pull=$(git status | grep -i "Your branch is behind")
+need_to_pull=$(git status | grep -i "Your branch is behind")
 popd
 
-if [ ! -z "$neec_to_pull" ]; then
+if [ ! -z "$need_to_pull" ]; then
     echo "Need to pull"
 
     # re-build blog
     sudo rm -rf /opt/blog
     sudo mkdir -p /opt/blog
     sudo git clone --branch=gh-pages https://github.com/alecmaly/blog.git /opt/blog
-    
+
     pushd /opt/blog
     # build tags
-    python3 tag_generator.py 
+    python3 tag_generator.py
 
     docker run -v "/var/www/html:/var/www/html" -v "$(pwd):/opt/blog" blog /opt/blog/docker_build.sh
-    popd 
+    popd
 
     # rebuild search index
     pushd /opt/alecmaly.com/services/blog_search_service/
@@ -41,5 +40,6 @@ else
     echo "No action necessary"
 fi
 
+echo "killing id: $SSH_AGENT_PID"
 # Kill ssh-agent process
 kill $SSH_AGENT_PID
