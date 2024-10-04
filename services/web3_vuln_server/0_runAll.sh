4bb3763a79789664179c2e8d370da115e5babcb3
Alec Maly <alecjmaly@gmail.com>
2024-10-04 08:48:07 -0400

    set full path for script that runs in cron

--- services/web3_vuln_server/0_runAll.sh
+++ services/web3_vuln_server/0_runAll.sh
@@ -1,15 +1,15 @@
 mkdir -p contract_monitoring
 
-python3 "1_collect-scopes.py"
+/usr/local/bin/python3 "1_collect-scopes.py"
 #### .".\2_getGithubDates.ps1"   # may not need anymore
-pwsh -noprofile ".\3_getGithubRepoCommitDetails.ps1"
-pwsh -noprofile ".\4_converToHTML.ps1"
-python3 "6_extractLiveContracts.py"
-python3 "7_lookupProxies.py"
-python3 "8_updateContractsList.py"
+/app/powershell/pwsh -noprofile ".\3_getGithubRepoCommitDetails.ps1"
+/app/powershell/pwsh -noprofile ".\4_converToHTML.ps1"
+/usr/local/bin/python3 "6_extractLiveContracts.py"
+/usr/local/bin/python3 "7_lookupProxies.py"
+/usr/local/bin/python3 "8_updateContractsList.py"
 # pwsh -noprofile "9_addPatchDiffDetails.ps1"
 
-pwsh -noprofile "11_generateHTMLReview.ps1"
+/app/powershell/pwsh -noprofile "11_generateHTMLReview.ps1"
 
 cp web3contractreport.html public
 cp web3repoupdates.html public
