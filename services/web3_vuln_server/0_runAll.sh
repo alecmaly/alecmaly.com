e91bbc87e3a3f0cdb30eddf0745c94037b2c186e
Alec Maly <alecjmaly@gmail.com>
2024-10-07 22:39:34 -0400

    getting environment variables into /etc/environment for powershell scripts

--- services/web3_vuln_server/0_runAll.sh
+++ services/web3_vuln_server/0_runAll.sh
@@ -2,8 +2,8 @@ mkdir -p contract_monitoring
 
 /usr/local/bin/python3 "1_collect-scopes.py"
 #### .".\2_getGithubDates.ps1"   # may not need anymore
-/app/powershell/pwsh -noprofile ".\3_getGithubRepoCommitDetails.ps1"
-/app/powershell/pwsh -noprofile ".\4_converToHTML.ps1"
+/app/powershell/pwsh -noprofile "3_getGithubRepoCommitDetails.ps1"
+/app/powershell/pwsh -noprofile "4_converToHTML.ps1"
 /usr/local/bin/python3 "6_extractLiveContracts.py"
 /usr/local/bin/python3 "7_lookupProxies.py"
 /usr/local/bin/python3 "8_updateContractsList.py"
