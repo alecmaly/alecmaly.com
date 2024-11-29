56b6f773f346924917072b1c01652a5b1b62cf05
Alec Maly <alecjmaly@gmail.com>
2024-11-28 19:27:56 -0500

    updated atom feed

--- services/github_rss/server.js
+++ services/github_rss/server.js
@@ -15,7 +15,7 @@ async function generateGithubHistoryRSS(res, url) {
         // console.log(html)
 
         let rss = `<?xml version="1.0" encoding="utf-8"?>
-        <rss version="2.0">
+        <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
             <channel>
                 <title>${github_username} - ${github_repo}: github updates</title>
                 <link>${url}</link>
